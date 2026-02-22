bdb7129067239849c7dc3a6b93d8f7c3e6bb2c8f
James Reagan <james@jpreagan.com>
2026-02-21 20:32:07 -1000

    update fish config

--- darwin/.config/fish/config.fish
+++ darwin/.config/fish/config.fish
@@ -28,11 +28,8 @@ if test -f $HOME/.cargo/env.fish
 end
 
 # Python
-fish_add_path --path /Library/Frameworks/Python.framework/Versions/Current/bin
-
-# Google Cloud SDK path
-if test -f $HOME/.local/google-cloud-sdk/path.fish.inc
-    source $HOME/.local/google-cloud-sdk/path.fish.inc
+if test -d /Library/Frameworks/Python.framework/Versions/Current/bin
+    fish_add_path --path /Library/Frameworks/Python.framework/Versions/Current/bin
 end
 
 # pnpm
@@ -58,20 +55,6 @@ end
 if status is-interactive
     set -g fish_greeting
     fish_vi_key_bindings
-
-    # Google Cloud SDK completions
-    complete -c gcloud -f -a '(__fish_argcomplete_complete gcloud)'
-    complete -c gsutil -f -a '(__fish_argcomplete_complete gsutil)'
-
-    # AWS CLI completion
-    if command -q aws; and command -q aws_completer
-        complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
-    end
-
-    # Azure CLI completion
-    if command -q az
-        complete --command az --no-files --arguments '(__fish_argcomplete_complete az)'
-    end
 
     if command -q starship
         starship init fish | source
--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -25,11 +25,6 @@ if test -f $HOME/.cargo/env.fish
     source $HOME/.cargo/env.fish
 end
 
-# Google Cloud SDK path
-if test -f $HOME/.local/google-cloud-sdk/path.fish.inc
-    source $HOME/.local/google-cloud-sdk/path.fish.inc
-end
-
 # OpenCode
 if test -d $HOME/.opencode/bin
     fish_add_path --path $HOME/.opencode/bin
@@ -47,20 +42,6 @@ end
 if status is-interactive
     set -g fish_greeting
     fish_vi_key_bindings
-
-    # Google Cloud SDK completions
-    complete -c gcloud -f -a '(__fish_argcomplete_complete gcloud)'
-    complete -c gsutil -f -a '(__fish_argcomplete_complete gsutil)'
-
-    # AWS CLI completion
-    if command -q aws; and command -q aws_completer
-        complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
-    end
-
-    # Azure CLI completion
-    if command -q az
-        complete --command az --no-files --arguments '(__fish_argcomplete_complete az)'
-    end
 
     if command -q starship
         starship init fish | source
